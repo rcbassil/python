a6a088478f50b343a54018b7d7f7987ee65690c6
Rodrigo Bassil <rcbassil@gmail.com>
2024-04-27 21:32:16 -0400

    Adding examples

--- parsetree.py
+++ parsetree.py
@@ -50,6 +50,36 @@ def evaluate(parseTree):
         return fn(evaluate(leftC),evaluate(rightC))
     else:
         return parseTree.getRootVal()
+    
+
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+def inorder(tree):
+    if tree:
+        inorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        inorder(tree.getRightChild())
+
+def postorder(tree):
+    if tree:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+def printexp(tree):
+  sVal = ""
+  if tree:
+      if tree.getLeftChild() != None:
+        sVal = '(' + printexp(tree.getLeftChild())
+      sVal = sVal + str(tree.getRootVal())
+      if tree.getRightChild() != None:
+        sVal = sVal + printexp(tree.getRightChild())+')'
+  return sVal
+
 
 
 
@@ -58,3 +88,14 @@ print(evaluate(pt))
 
 pt = buildParseTree("( 3 + ( 4 * 5 ) )")
 print(evaluate(pt))
+
+print("preorder")
+preorder(pt)
+
+print("inorder")
+inorder(pt)
+
+print("postorder")
+postorder(pt)
+
+print(printexp(pt))
