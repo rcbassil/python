52266d14e065e7797904a0c90a942156e759f491
Rodrigo Bassil <rcbassil@gmail.com>
2024-04-27 20:46:47 -0400

    Adding examples

--- algorithms.py
+++ algorithms.py
@@ -291,6 +291,14 @@ print(H[26])
 
 
 
+# trees
+
+my_tree = ['a', ['b', ['d',[],[]], ['e',[],[]] ], ['c', ['f',[],[]], []] ]
+print(my_tree)
+print('left subtree = ', my_tree[1])
+print('root = ', my_tree[0])
+print('right subtree = ', my_tree[2])
+
 
 
 
---
+++ binaryTreeExample2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from BinaryTree import BinaryTree
+
+r = BinaryTree('a')
+print(r.getRootVal())
+print(r.getLeftChild())
+r.insertLeft('b')
+print(r.getLeftChild())
+print(r.getLeftChild().getRootVal())
+r.insertRight('c')
+print(r.getRightChild())
+print(r.getRightChild().getRootVal())
+r.getRightChild().setRootVal('hello')
+print(r.getRightChild().getRootVal())
