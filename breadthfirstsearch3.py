167c6620246a8e98cf55884f62dbb6e2ab341f63
Rodrigo Bassil <rcbassil@gmail.com>
2024-04-28 23:22:41 -0400

    Adding examples

---
+++ breadthfirstsearch3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+def bfs(graph, node):
+  
+  queue = []     #Initialize a queue
+  visited = [] # List to keep track of visited nodes.
+
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+# Driver Code
+bfs(graph, 'A')
