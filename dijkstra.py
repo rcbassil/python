4cc4b7465e04662a00ba7dec0c2af55eb543b9bb
Rodrigo Bassil <rcbassil@gmail.com>
2024-04-29 10:31:23 -0400

    Adding examples

--- dijkstra.py
+++ dijkstra.py
@@ -9,16 +9,26 @@ def dijkstra(aGraph,start):
     pq = PriorityQueue()
     start.setDistance(0)
     pq.buildHeap([(v.getDistance(),v) for v in aGraph])
+    print(pq.heapArray)
     while not pq.isEmpty():
         currentVert = pq.delMin()
         for nextVert in currentVert.getConnections():
             newDist = currentVert.getDistance() \
                     + currentVert.getWeight(nextVert)
             if newDist < nextVert.getDistance():
-                nextVert.setDistance( newDist )
+                nextVert.setDistance(newDist)
                 nextVert.setPred(currentVert)
+                # This method is used when the distance to a vertex that is already in the queue is reduced,
+                # and thus moves that vertex toward the front of the queue.
                 pq.decreaseKey(nextVert,newDist)
 
+
+def traverse(y):
+    x = y
+    while (x.getPred()):
+        print(x.key)
+        x = x.getPred()
+    print(x.key)
 
 
 g = Graph()
@@ -45,11 +55,17 @@ g.add_edge("y","z",1)
 g.add_edge("w","z",5)
 
 for v in g:
-    print(v)
+    print("({}, distance {} )".format(v, v.getDistance()))
+    for w in v.getConnections():
+       print("({} -> {}, weight {})".format(v.key, w.key, v.getWeight(w)))
+
+dijkstra(g,g.get_vertex("u"))
+
+for v in g:
+    print("({}, distance {} )".format(v, v.getDistance()))
     for w in v.getConnections():
         print("({} -> {}, weight {})".format(v.key, w.key, v.getWeight(w)))
 
 
+traverse(g.get_vertex('z'))
 
-
-dijkstra(g,g.get_vertex("u"))
